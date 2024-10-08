9734c17907c5c6b725b6a36d07c7e9859caafdd5
Sauciuc Matei <matei@goosfraba.ro>
2024-10-08 17:54:41 +0300

    Seminar3

--- Seminar2/src/main/java/com/example/seminar1/MainActivity.java
+++ Seminar2/src/main/java/com/example/seminar1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.seminar1;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Activitate","onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activitate","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("Activitate","onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Activitate","onResume");
+        Toast.makeText(this, R.string.mesaj, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf("Activitate","onDestroy");
     }
 }
